2c933b41d50117ca41c370c38b3ae9512d11eadf
Sazuna <liza.fretel@gmail.com>
2023-11-09 17:47:50 +0100

    Tokenization of a Machine Turing program and main file.

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+import tokenize
+
+def main(file):
+	with open(file, 'r', encoding='ISO-8859-1') as f:
+		program = f.read()
+	program, lines_number = tokenize.clean_lines(program)
+	tokens = tokenize.make_tokens(program, lines_number)
+	print(tokens)
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print("Il manque le fichier TS.")
+		print("Usage:\n\tpython main.py FICHIER.TS")
+		sys.exit(1)
+	main(sys.argv[1])
---
+++ tokenize.py
@@ -0,0 +1,42 @@
+import regex
+
+from typing import List
+
+def clean_lines(text: str):
+	# split lines
+	text = regex.split(r"\r\n|\n", text)
+	new_text = []
+	lines_number = []
+	line_n = 0
+	for line in text:
+		line_n += 1
+		# sometimes, } is not separated from its previous word
+		line = regex.sub("}", " }", line)
+		line = regex.sub("\(", " (", line)
+
+		new_line = ""
+		for token in line.split():
+			token = token.strip()
+			if len(token) > 0 and token[0] == '%':
+				break
+				# ignore comment lines or inline comments
+			new_line += token + " "
+		if len(new_line) > 0:
+			new_text.append(new_line)
+			lines_number.append(line_n)
+	return (new_text, lines_number)
+
+def make_tokens(lines: List[str], lines_number: List[int]):
+	"""
+	Cut lines into tokens (token, line_number, instruction_number).
+	"""
+	tokens = []
+	instruction_n = 0
+	assert(len(lines) == len(lines_number))
+	for line, line_n in zip(lines, lines_number):
+		for token in regex.findall(r'((si \((0|1)\))|[^ ]+)', line):
+			token = token[0]
+			instruction_n += 1
+			tokens.append((token, line_n, instruction_n))
+	return tokens
+	
